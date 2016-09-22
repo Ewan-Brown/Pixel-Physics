38d11ecfccb90ace06003da11478a80b73fcc870
Ewan-Brown <ewanbrown2000@gmail.com>
2016-09-22 18:58:33 -0400

    Change size of particles, deleted old methods
    Can now change the size of particles with , and .
    Deleted old stuff.

--- PixelPhysics/src/stuff/Particle.java
+++ PixelPhysics/src/stuff/Particle.java
@@ -9,15 +9,13 @@ public class Particle {
 	public Color color = Color.GRAY;
 	public double x;
 	public double y;
-	public int width = 1;
-	public int height = 1;
 	public double speedX;
 	public double speedY;
 	public double tempSpeedY;
 	public double tempSpeedX;
 	public static Random rand = new Random();
 	
-	public Particle(int x, int y, double speedX,double speedY){
+	public Particle(int x, int y, double speedX,double speedY,int size){
 		this.x = x;
 		this.y = y;
 		this.speedX = speedX;
@@ -28,18 +26,5 @@ public class Particle {
 	}
 	public Point2D getPoint(){
 		return new Point2D.Double(x,y);
-	}
-	public double getLeftSide(){
-		return (this.x - (this.width - 1) / 2);
-	}
-	public double getRightSide(){
-		return (this.x +(this.width - 1) / 2);
-		
-	}
-	public double getTopSide(){
-		return (this.y + (this.height - 1) / 2);
-	}
-	public double getBottomSide(){
-		return(this.y -(this.height - 1) / 2);
 	}
 }
